cee1c22fd7ed04d65861b28b3154d5a23c8f9eea
Flavio Ever <flavio.ever@outlook.com>
2020-12-14 23:30:20 -0300

    Colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { Container, Button } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme} from '@material-ui/core';
+
+
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#f44336',
+      },
+      secondary: {
+        main: '#9c27b0',
+      }
+    },
+  });
+
   return (
     <div className="App">
-      <Button variant="contained" color="primary">
-        Hello World
-    </Button>
+      {/**
+       * https://material-ui.com/pt/api/container/
+       * https://material-ui.com/pt/components/grid/
+       * https://material-ui.com/pt/guides/interoperability/ (styled-component)
+       * 
+       * https://material-ui.com/pt/customization/theming/ <----
+       * https://material-ui.com/pt/customization/default-theme/ <----
+       * https://material-ui.com/pt/customization/color/ <---
+       */
+      }
+      <ThemeProvider theme={theme}>
+      <Container maxWidth="xl">
+      
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </Container>
+      </ThemeProvider>
     </div>
   );
 }
